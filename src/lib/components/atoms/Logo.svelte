bbbb9142f32057d8692de296c36685487de7532f
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-13 16:00:26 -0500

    Update logo text to include a link

--- src/lib/components/atoms/Logo.svelte
+++ src/lib/components/atoms/Logo.svelte
@@ -18,7 +18,8 @@
 			><tspan x="275.57227" y="292.17088" id="tspan1319"
 				><tspan
 					style="font-size:106.667px;font-family:YoungSerif;"
-					id="tspan1317">Space Outlawz</tspan
+					id="tspan1317"><a class="logo" href="/images/IMG_1279.webp" aria-label="Site logo">
+			<Logo /></tspan
 				></tspan
 			></text
 		>
