849b6861401941042958d413c6730260271ca857
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-13 13:00:39 -0500

    Update meta.ts

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -14,8 +14,8 @@ export const keywords = [
 ];
 
 export const description =
-	"A light, neat, and easy-to-use SvelteKit template for your next website.";
+	"A blog from The Space Outlaws";
 
-export const title = 'SvelteKit Static Blog Template';
+export const title = 'Space outlawz blog';
 
 export const image = `${siteBaseUrl}/images/site-preview.png`;
