0e12670d0a480e6b3f46b94fa7dd0505051deca5
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-13 15:39:30 -0500

    Update About.svelte

--- src/lib/components/organisms/About.svelte
+++ src/lib/components/organisms/About.svelte
@@ -1,6 +1,5 @@
 <script>
 	import SparklingHighlight from '$lib/components/molecules/SparklingHighlight.svelte';
-	import Socials from '$lib/components/molecules/Socials.svelte';
 	import Image from '../atoms/Image.svelte';
 </script>
 
